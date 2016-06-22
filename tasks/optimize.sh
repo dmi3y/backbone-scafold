8be20278b5cda693c53a295ff8f54c1702556ca2
Dmitry Lapshukov <lapshukov@gmail.com>
2016-06-22 11:35:57 -0700

    fixing up folder name and paths

--- tasks/optimize.sh
+++ tasks/optimize.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-r.js -o /config/optimize.js
+r.js -o ./config/optimize.js
 
 # replace require-config.js to index.js
 # due to optimization took care of it already
-cd deploy
-	sed -i.bak 's/require-config/index/' index.html
-	rm index.html.bak
-	rm script/require-config.js
+cd ./deploy
+	sed -i.bak 's/require-config/index/' ./index.html
+	rm ./index.html.bak
+	rm ./scripts/require-config.js
 cd -
