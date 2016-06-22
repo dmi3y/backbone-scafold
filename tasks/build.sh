27bba7165812f3cd43bd65535323bf962088eb58
Dmitry Lapshukov <lapshukov@gmail.com>
2016-06-22 11:33:29 -0700

    build index  consecutively with compile templates

--- tasks/build.sh
+++ tasks/build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 npm run copy-dependencies & \
-npm run compile-templates & \
+npm run compile-templates && npm run build-index & \
 npm run compile-scripts & \
-npm run compile-styles & \
-npm run build-index &
+npm run compile-styles &
 
 wait
